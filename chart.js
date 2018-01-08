356befe36d87e09b9a5d03b4745dc895830465c6
Erin St. Peter <ers178@student.hks.harvard.edu>
2018-01-08 18:15:08 -0500

    pulled out the chart font and put it in a separate style category

--- chart.js
+++ chart.js
@@ -29,6 +29,7 @@ var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
+    .attr("id", "apple-stock-chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("data.tsv", function(error, data) {
